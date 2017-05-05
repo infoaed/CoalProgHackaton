983e7a00e213dce20d9daeb343e08e779b26fdb1
indrkl <indrkl@gmail.com>
2017-05-05 11:53:40 +0300

    First progress

--- genericFunctions.py
+++ genericFunctions.py
@@ -1,16 +1,55 @@
-import re
+# -*- coding: utf-8 -*-
 
+import re
+import psycopg2
+import datetime
 
 class base():
+	def __init__(self):
+		self.wordDict = {}
 	
-	def getBaseWord(word):
+	def getBaseWord(self, word):
 		return word
 	
-	def processText(text):
+	def processText(self, text):
 		words = re.split('. | |, |.\n', text)
 		returnWords = {}
 		for word in words:
-			w = getBaseWord(word)
+			w = self.getBaseWord(word)
+			if w not in returnWords:
+				returnWords[w] = {}
 			returnWords[w]['cnt'] = returnWords[w].get('cnt', 0) + 1
 			# lisa tegevused.
 		return returnWords
+	
+	ConnectionParams = {
+		'dbname': 'coalprog2',
+		'user': 'postgres',
+		'password': 'test',
+		}
+	
+	def addToWordDict(self, tmpDict):
+		for key in tmpDict:
+			if key not in self.wordDict:
+				self.wordDict[key] = {}
+			self.wordDict[key]['cnt'] = self.wordDict[key].get('cnt', 0) + tmpDict[key]['cnt']
+			
+	
+	def testProcessing(self):
+		conn = psycopg2.connect(**base.ConnectionParams)
+		cur = conn.cursor()
+
+		fromDate = datetime.date(2010, 1, 1) # Year, Month, Day
+		toDate = datetime.date(2011, 1, 1) # Year, Month, Day
+
+		cur.execute("""SELECT tekst from public.syndmus where kuupaev > %s and kuupaev < %s LIMIT 10""", (fromDate, toDate))
+		
+		rows = cur.fetchall()
+		for row in rows:
+			self.addToWordDict(self.processText(row[0]))
+		print str(self.wordDict)
+		
+B = base()
+B.testProcessing()
+		
+	
--- postgretest.py
+++ postgretest.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import psycopg2
+import datetime
 
 params = {
-  'dbname': 'coalprog',
-  'user': 'indrek',
-  'password': 'korru',
-  'host': '172.16.1.245',
-  'port': 5432
+  'dbname': 'coalprog2',
+  'user': 'postgres',
+  'password': 'test',
 }
 
 
@@ -15,11 +16,22 @@ print 'test'
 
 cur = conn.cursor()
 
-cur.execute("""SELECT *
-FROM information_schema.columns
-WHERE table_name = 'SYNDMUS'""")
-cur.fetchall()
+fromDate = datetime.date(2010, 1, 1) # Year, Month, Day
+toDate = datetime.date(2011, 1, 1) # Year, Month, Day
 
-cur.execute("""SELECT * from public.syndmus""")
+cur.execute("""SELECT tekst from public.syndmus where kuupaev > %s and kuupaev < %s LIMIT 10""", (fromDate, toDate))
 
 print cur.fetchone()
+
+"""
+syndmus - [('idsyndmus',), ('kuupaev',), ('loodud',), ('esineja',), ('tekst',), ('jrk',), ('must_steno',), ('link',), ('ems_id',), ('liik',), ('paevakord_id',), ('aeg_video_suhtes',), ('video_nahtav',), ('video_id',)]
+p'evakord - [('idpaevakord',), ('kuupaev',), ('loodud',), ('pealkiri',), ('link',), ('idistung',), ('must_steno',), ('liik',), ('ems_id',), ('toimetatud',), ('aeg_video_suhtes',), ('video_id',)]
+istung - [('idistung',), ('istungjark_cf',), ('riigikogu_cf',), ('kuupaev',), ('loodud',), ('kellaaeg',), ('must_steno',), ('lopetatud',), ('istunginfo',), ('videod_nahtavad',)]
+
+"""
+
+"""
+1) Sõna tuumad leida
+2) Koostada tabel, kus on indekseeritud sõnade esinemised. Esialgu kõigest loetud arv.
+3) Töötada läbi syndmus andmebaas.
+"""

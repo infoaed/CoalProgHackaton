76bf1dc97da5ba156293f76b4690bfd77fcc1547
Ilja Samoilov <ilsamoilov@gmail.com>
2017-05-20 23:22:40 +0300

    Now search page with new and beautiful design

--- garage48/js/js.js
+++ garage48/js/js.js
@@ -1,31 +1,40 @@
 var results = $("#results");
+var count = 0;
 function search() {
     startSpinner();
+    count=0;
     results.empty();
     var mandatory = $("#mandatory").val().replace(/ /g, '');
     var optional = $("#optional").val().replace(/ /g, '');
     $.getJSON("http://193.40.11.124/test/index.py?mandatory=" + mandatory + "&optional=" + optional, function (data) {
         var items = [];
         $.each( data, function( key, val ) {
-            var str = "<li id='" + key + "'><b>" + val.title + "</b></li>";
-            items.push(str);
+            items.push("<div class='divide64'></div>");
+            items.push("<div class='group draft'>");
+            // var str = "<li id='" + key + "'><b>" + val.title + "</b></li>";
+            items.push("<div class='element event-title'><b>" + key + val.title + "</b></div>");
+            items.push("<div class='divide8'></div>")
             var events = val.events;
-            $.each( events, function (key, val) {
-                var text = "<p>Event id: " + key + "</p><p class='inner-text'>" + val.text + "</p>"
-                items.push(text);
-                var words = "<ul>"
-                $.each(val.words, function (key, val) {
-                    words += "<li>" + key
-                        + "<p>total: " + val.totalcount + "</p>"
-                        + "<p>event: " + val.eventcount + "</p>"
-                    + "</li>";
-                });
-                words += "</ul>";
-                items.push(words);
-            })
-        });
 
-        $( "<ul/>", {
+            $.each( events, function (key, val) {
+                count += 1;
+                var text = "<div class='element event-id'>Sündmuse id: " + key + "</div><div class='element event-content inner-text'>" + val.text + "</div>"
+                items.push(text);
+                // var words = "<ul>"
+                $.each(val.words, function (key, val) {
+                    //     words += "<li>" + key
+                    //         + "<p>total: " + val.totalcount + "</p>"
+                    //         + "<p>event: " + val.eventcount + "</p>"
+                    //     + "</li>";
+                });
+                // words += "</ul>";
+                // items.push(words);
+                items.push("<div class='divide16'></div><div class='border'></div>")
+            })
+            items.push("</div>")
+        });
+        $("#count").text(count);
+        $( "<div/>", {
             html: items.join( "" )
         }).appendTo("#results");
 
@@ -33,8 +42,8 @@ function search() {
             highlightWords(mandatory + "," + optional);
         } else {
             highlightWords(mandatory);
-            console.log("HELLO")
         }
+        $(".draft-count").show();
         stopSpinner();
     })
 

94f2af0ff9d3abe09f563dfe77337f7e3a2292ca
artur <artur.salus@gmail.com>
2017-05-21 14:17:06 +0300

    Add json cache

--- garage48/index.py
+++ garage48/index.py
@@ -4,18 +4,30 @@ import cgitb
 import cgi
 from estonianBase import estonianBase
 import json
+import hashlib
+from pathlib import Path
 
 cgitb.enable()
 print("Content-Type: application/json; charset=utf-8\n\n")
 
 form = cgi.FieldStorage()
 mandatory = str(form.getvalue('mandatory')).split(',')
+mandatory.sort()
 optional = str(form.getvalue('optional')).split(',')
+optional.sort()
 
-#mandatory = ['metsaregister']
-#optional = ['raieviiside']
+cacheFileString = ','.join(mandatory) + ','.join(optional)
+cacheFileMd5 = hashlib.md5(cacheFileString.encode('utf-8')).hexdigest()
+cacheFilePath = 'cache/' + cacheFileMd5 + '.cache'
+file = Path(cacheFilePath)
+if not file.is_file():
+	Base = estonianBase()
+	data = Base.findData(mandatory, optional)
+	jsonString = json.dumps(data)
+	cacheFile = open(cacheFilePath, 'w+')
+	cacheFile.write(jsonString)
+	cacheFile.close()
+	
+cacheFile = open(cacheFilePath)
+print(cacheFile.read())
 
-Base = estonianBase()
-data = Base.findData(mandatory, optional)
-
-print (json.dumps(data))

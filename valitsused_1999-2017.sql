45a98c2f65ddc8d0b6fd9b6a605cc4efcf798cfb
Märt Põder <boamaod@gmail.com>
2017-05-24 22:15:10 +0300

    Add governments in sql.

---
+++ valitsused_1999-2017.sql
@@ -0,0 +1,9 @@
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Siim Kallase valitsus','2002-01-28','2003-04-10');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Juhan Partsi valitsus','2003-04-10','2005-04-13');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Andrus Ansipi esimene valitsus','2005-04-13','2007-04-05');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Andrus Ansipi teine valitsus','2007-04-05','2011-04-06');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Andrus Ansipi kolmas valitsus','2011-04-06','2014-03-26');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Taavi Rõivase esimene valitsus','2014-03-26','2015-04-09');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Taavi Rõivase teine valitsus','2015-04-09','2016-11-22');
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Jüri Ratase valitsus','2016-11-22',NULL);
+INSERT INTO koalitsioon(nimi,algus,l6pp) VALUES ('Urmas Klaasi (Urmas Kruuse) linnavalitsus',' 2013-10-29',NULL);
